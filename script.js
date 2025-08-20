0eb467b703e4e99d5780a0bbe60a393973d0b85a
Doston Jokhongirov <dostonjokhongirov@gmail.com>
2025-08-20 18:07:50 +0500

    add-del

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ for (let modal = prompt("eppend by 'add, ', delete by 'del, ' and finish by 'sto
     if (modal) {
         arr = modal.split(' ')
     }
-    if (arr[0] == 'add,') {
+    if (arr[0] == 'add,' && arr.length) {
         newArr.push(arr[1])
         console.log(newArr);
-    }else if (arr[0] == 'del,') {
+        arr = []
+    }else if (arr[0] == 'del,' && arr.length) {
         newArr.splice(newArr.indexOf(arr[1]), 1)
         console.log(newArr);
+        arr = []
     }
     modal = prompt("eppend by 'add, ', delete by 'del, ' and finish by 'stop'")
 }
